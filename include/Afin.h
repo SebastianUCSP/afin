ccefe0cce913603bad235144f3e42473a7077304
SebastianUCSP <42581071+SebastianUCSP@users.noreply.github.com>
2019-04-13 13:26:29 -0500

    Add files via upload

---
+++ include/Afin.h
@@ -0,0 +1,28 @@
+#ifndef CESAR_H
+#define CESAR_H
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+
+using namespace std;
+
+class Afin
+{
+    public:
+        Afin();
+        int modulo(int, int);
+        string encriptar(string );
+        string desencriptar(string );
+        int EuclidianRecursive(int m, int n);
+        int GenerarA(int);
+    //private:
+        string alfabeto = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; //64
+        int claveA = EuclidianRecursive(rand(100), );
+        int claveB =
+
+
+
+};
+
+#endif // CESAR_H
---
+++ src/Afin.cpp
@@ -0,0 +1,34 @@
+#include "Afin.h"
+
+Afin::Afin()
+{
+
+}
+
+int Afin::modulo(int a, int b){
+    int aux = a + ((abs(a/b)+1)*b);
+    return aux - ((aux/b)*b);
+}
+
+int Afin::EuclidianRecursive(int m, int n){
+    if(n == 0)
+        return m;
+    else
+        return EuclidianRecursive(n, modulo(m,n));
+}
+
+int Afin::GenerarA(int n)
+{
+    int random = (rand()%alfabeto.size());
+    if()
+}
+
+string Afin::encriptar(string mensaje)
+{
+
+}
+
+string Afin::desencriptar(string mensaje)
+{
+
+}
